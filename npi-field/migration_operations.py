67b1e523b9df4ddb6bfcc4aee0e09dacb1779ffc
Kyle Jensen <20227912+PhoenixStorm1015@users.noreply.github.com>
2024-06-17 21:59:01 -0400

    Started custom operations; GPT sucks donkey balls

--- npi-field/migration_operations.py
+++ npi-field/migration_operations.py
@@ -1,17 +1,17 @@
+from django.db import migrations, models
 from django.db.migrations.operations.base import Operation
-from django.db.migrations.operations.models import CreateModel
-from django.db.migrations.writer import MigrationWriter
+from fields import NPIField
 
 
 class CreateNPIDomain(Operation):
     """Creates custom domain for NPI ONLY ON POSTGRES ENGINE."""
-
     def state_forwards(self, app_label, state):
         pass
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         if schema_editor.connection.vendor == "postgresql":
-            schema_editor.execute("")
+            schema_editor.execute("")  # TODO: Create Function
+            schema_editor.execute("")  # TODO: Create Domain
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
         if schema_editor.connection.vendor == "postgresql":
@@ -21,6 +21,5 @@ class CreateNPIDomain(Operation):
     def describe(self):
         return "Creates a custom NPI Domain with Luhn algorithm validation only on PostgreSQL Engine"
 
-
-class CustomModelCreation(CreateModel):
-    pass
+    def migration_name_fragment(self):
+        return "create_npi_domain"
